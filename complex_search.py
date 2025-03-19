d03ef04faecf3dff8fa9d51b42c1d60610e17596
laksanautama <kussa_laksana@uhnsugriwa.ac.id>
2025-03-20 00:49:16 +0800

    Update complex_search.py
    updating moving_state and valuation func

--- complex_search.py
+++ complex_search.py
@@ -75,20 +75,23 @@ class ComplexProblem():
     #function for getting the total number of 
     # confronted queens in horizontal & diagonal direction
     def valuation(self, queen_group):
-        reached = deque()
+        reached_h = deque()
+        reached_d = deque()
         sum = 0
         
         for q in queen_group:
-            print(f"queen saat ini: {q.get_name()}")
+            # print(f"queen saat ini: {q.get_name()}")
 
-            if q not in reached:
-                dc, reached = self.diagonal_search(q, queen_group, reached)
-                hc, reached = self.horizontal_search(q, queen_group, reached)
-                print(f"diagonal cost saat ini: {dc}")
-                print(f"horizontal cost saat ini: {hc}")
-                sum = sum + dc + hc
+            if q not in reached_d:
+                dc, reached_d = self.diagonal_search(q, queen_group, reached_d)
+                # print(f"diagonal cost saat ini: {dc}")
+                sum = sum + dc
+            if q not in reached_h:
+                hc, reached_h = self.horizontal_search(q, queen_group, reached_h)
+                #print(f"horizontal cost saat ini: {hc}")
+                sum = sum + hc
 
-            print(f"sum saat ini: {sum}")
+            #print(f"jumlah queen berlawanan saat ini: {sum}")
 
         return sum
 
@@ -96,34 +99,49 @@ class ComplexProblem():
     #value by moving single queen in its column
     def moving_state(self, queen_group):
         h = math.inf
-        state_candidate = None
+        current_group = queen_group
+        list_Y = []
 
-        for q in queen_group:
+        for q in current_group:
             orig_posY = q.get_posY()
 
-            for i in range(7):
+            for i in range(self.get_maxY()):
                 q.set_posY(q.get_posY()+1)
-                value = valuation(queen_group)
-
-                if value <= h:
+                value = self.valuation(current_group)
+                
+                if value < h:
                     h = value
-                    state_candidate = queen_group
+                                        
+                    list_Y.clear()
+                    for c in current_group:
+                        list_Y.append(c.get_posY())
                 
                 if q.get_posY() == self.get_maxY():
                     q.set_posY(orig_posY)
 
-        return state_candidate
+        for i in range(len(current_group)):
+            current_group[i].set_posY(list_Y[i])
+        
+        return current_group, h
     
     #Hill-Climbing Algorithm
     def hill_cimbing(self, queen_group):
         p = True
-
+        
+        current_group = queen_group
+        current_value = self.valuation(queen_group)
+        
         while p:
-            current_value = self.valuation(queen_group)
-            queen_group, value = self.moving_state(queen_group)
+            
+            next_group, next_value = self.moving_state(current_group)
+                                   
+            if next_value < current_value:
+                current_value = next_value
+                current_group = next_group
+                break
         
+        return current_group, current_value
         
-        pass 
     
 
 class Queen():
@@ -149,6 +167,86 @@ class Queen():
     
 #====== DUMMY FUNC==========#
 
+# def horizontal_search(q, queen_group, reached):
+#     sum = 0
+#     hc = 0
+
+#     for x in queen_group:
+#         if q != x:
+#             if x.get_posY() == q.get_posY():
+#                 sum+=1
+#                 reached.append(x)
+#     if sum >= 2:
+#         sum+=1
+#         hc = (math.factorial(sum))/(math.factorial(2))*math.factorial(sum-2)
+        
+#     elif sum == 1:
+#         hc = 1
+
+#     else:
+#         hc = sum
+
+#     return hc, reached
+
+#     #function for getting total number of queens diagonally
+# def diagonal_search(q, queen_group, reached):
+#     sum_pos = 0
+#     sum_neg = 0
+#     dc = 0
+
+#     for x in queen_group:
+#         if q!= x:
+#             m = (x.get_posY()-q.get_posY())/(x.get_posX()-q.get_posX())
+#             if(m == 1):
+#                 sum_pos+=1
+#                 reached.append(x)
+#             elif(m == -1):
+#                 sum_neg+=1
+#                 reached.append(x)
+
+#     if sum_pos >= 2:
+#         sum_pos+=1
+#         dc+= (math.factorial(sum_pos))/(math.factorial(2))*math.factorial(sum_pos-2)
+        
+#     elif sum_pos == 1:
+#         dc+= 1    
+#     else:
+#         dc+= sum_pos
+
+#     if sum_neg >= 2:
+#         sum_neg+=1
+#         dc+= (math.factorial(sum_neg))/(math.factorial(2))*math.factorial(sum_neg-2)
+#     elif sum_neg == 1:
+#         dc+=1
+#     else:
+#         dc+= sum_neg
+
+#     return dc, reached
+
+#     #function for getting the total number of 
+#     # confronted queens in horizontal & diagonal direction
+# def valuation(queen_group):
+#     reached_h = deque()
+#     reached_d = deque()
+#     sum = 0
+        
+#     for q in queen_group:
+#         print(f"queen saat ini: {q.get_name()}")
+
+#         if q not in reached_d:
+#             dc, reached_d = diagonal_search(q, queen_group, reached_d)
+#             print(f"diagonal cost saat ini: {dc}")
+#             sum = sum + dc
+#         if q not in reached_h:
+#             hc, reached_h = horizontal_search(q, queen_group, reached_h)
+#             print(f"horizontal cost saat ini: {hc}")
+#             sum = sum + hc
+
+#         print(f"jumlah queen berlawanan saat ini: {sum}")
+
+#     return sum
+
+
 def main():
     queen_1 = Queen("Q1", 0, 3)
     queen_2 = Queen("Q2", 1, 2)
@@ -159,20 +257,30 @@ def main():
     queen_7 = Queen("Q7", 6, 1)
     queen_8 = Queen("Q8", 7, 2)
 
+    # queen_1 = Queen("Q1", 0, 6)
+    # queen_2 = Queen("Q2", 1, 4)
+    # queen_3 = Queen("Q3", 2, 2)
+    # queen_4 = Queen("Q4", 3, 5)
+    # queen_5 = Queen("Q5", 4, 6)
+    # queen_6 = Queen("Q6", 5, 4)
+    # queen_7 = Queen("Q7", 6, 7)
+    # queen_8 = Queen("Q8", 7, 2)
 
     queen_group = [queen_1, queen_2, queen_3, queen_4, queen_5, 
                    queen_6, queen_7, queen_8]
     queen_problem = ComplexProblem(max_x=7, max_y=7)
+    solution_state, value = queen_problem.hill_cimbing(queen_group)
+    # sum = valuation(queen_group)
+    # print(f"cost minimum: {sum}")
 
-    solution = queen_problem.moving_state(queen_group)
-    # grp = moving_state(queen_group)
-    for c in solution:
+    # solution = queen_problem.moving_state(queen_group)
+
+    for c in solution_state:
         print(f"nama: {c.get_name()}, posX: {c.get_posX()}, posY: {c.get_posY()}")
     
-
-    # cost = valuation(queen_group)
-    # print(f"best cost: {cost}")
+    print(f"value dari state paling optimal adalah: {value}")
 
 if __name__ == "__main__":
+
     main()
      
